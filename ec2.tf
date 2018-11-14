4e4dd6513daa9e9007b2c004b8ac66dbcf92881b
root <root@ip-172-16-0-12.us-east-2.compute.internal>
2018-11-14 18:48:36 +0000

    deleted:    ansible.pem 	new file:   challenge.pem 	new file:   devgrid-playbook/roles/jenkins/tasks/main.yml 	new file:   devgrid-playbook/roles/jenkins/vars/main.yml 	new file:   devgrid-playbook/site.retry 	new file:   devgrid-playbook/site.yml 	modified:   ec2.tf 	modified:   master.yml 	modified:   terraform.tfstate 	modified:   terraform.tfstate.backup

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,16 @@
 provider "aws" {
-	access_key    = "AKIAJ7L6XEMZM7MV7CCA"
-	secret_key = "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"
+	access_key    = "AKIAJPB5YTT4XXCJUPHA"
+	secret_key = "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"
 	region 	   = "us-east-2"
 }
 
 resource "aws_instance" "jenkins_master" {
 	ami				= "ami-0b59bfac6be064b78"
 	instance_type   		= "t2.micro"
-	key_name			= "ansible"
+	key_name			= "challenge"
 	associate_public_ip_address 	= true
-	subnet_id			= "subnet-0c88800b4d1404bad"
 	provisioner "local-exec" {
-        	command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ./ansible.pem -i '${aws_instance.jenkins_master.public_ip},' master.yml"
+        	command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ./challenge.pem -i '${aws_instance.jenkins_master.public_ip},' ./devgrid-playbook/site.yml"
         }	 
 }
 
