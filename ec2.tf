f46155e1dbdeb920ac25b87cdfd792dce6cb7bd2
root <root@ip-172-16-0-9.us-east-2.compute.internal>
2018-11-15 11:55:53 +0000

    new file:   Jenkinsfile 	modified:   devgrid-playbook/roles/jenkins/vars/main.yml 	modified:   devgrid-playbook/site.retry 	modified:   ec2.tf 	modified:   terraform.tfstate 	modified:   terraform.tfstate.backup

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-	access_key    =
-	secret_key = 
+	shared_credentials_file = "/root/.aws/credentials"
 	region 	   = "us-east-2"
 }
 
 resource "aws_instance" "jenkins_master" {
 	ami				= "ami-0b59bfac6be064b78"
 	instance_type   		= "t2.micro"
-	key_name			= "challenge"
+	key_name			= "ansibledevgrid"
+	subnet_id			= "subnet-0c88800b4d1404bad"
 	associate_public_ip_address 	= true
 	provisioner "local-exec" {
-        	command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ./challenge.pem -i '${aws_instance.jenkins_master.public_ip},' ./devgrid-playbook/site.yml"
+        	command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key /root/ansibledevgrid.pem -i '${aws_instance.jenkins_master.public_ip},' ./devgrid-playbook/site.yml"
         }	 
 }
 
