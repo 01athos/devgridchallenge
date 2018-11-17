8a06b8371b422be7bb4f45a0ed70c87bfd7d9523
root <root@ip-172-16-0-9.us-east-2.compute.internal>
2018-11-17 12:45:39 +0000

    modified:   devgrid-playbook/roles/jobs/files/createjob.sh

--- devgrid-playbook/roles/jobs/files/createjob.sh
+++ devgrid-playbook/roles/jobs/files/createjob.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080 create-job pipeline-devgrid < /tmp/jobjenkins.xml
+sleep 10
+java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ restart 
